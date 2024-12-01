2ea8feeebe7f3461537ae2d4db6f011717a6bfd5
gordonjun <gordonoh@yahoo.com.sg>
2024-12-01 16:43:16 +0800

    edited vercel.json

--- tg_bot/app.py
+++ tg_bot/app.py
@@ -3,8 +3,9 @@ import sys
 import os
 import time
 from flask import Flask, request, render_template
+
 from modules import modules
-# from handlers.routes import configure_routes
+from handlers.routes import configure_routes
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config import (TELEGRAM_BOT_TOKEN, TEST_TG_CHAT_ID, VERCEL_APP_URL,
@@ -17,61 +18,46 @@ OWNER_ID = OWNER_ID or os.getenv('OWNER_ID')
 
 bot = telebot.TeleBot(token=BOT_TOKEN, threaded=False)
 app = Flask(__name__)
-# configure_routes(app, bot, APP_URL)
+configure_routes(app, bot, APP_URL)
 
 
-@app.route("/")
-def index():
-    bot.remove_webhook()
-    time.sleep(1)
-    bot.set_webhook(url=APP_URL)
-
-    hello = modules.hello()
-    content = modules.content()
-    return render_template("index.html", hello=hello, content=content)
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    cid = message.chat.id
+    bot.send_message(
+        cid,
+        "Welcome to Memecoins Trading Toolkit!\nType /help to find all commands."
+    )
 
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    update = telebot.types.Update.de_json(
-        request.stream.read().decode("utf-8"))
-    bot.process_new_updates([update])
-    return "ok", 200
+@bot.message_handler(commands=['help'])
+def command_help(message):
+    cid = message.chat.id
+    help_text = "The following commands are available: \n"
+    for key in modules.COMMANDS:
+        help_text += '/' + key + ': '
+        help_text += modules.COMMANDS[key] + '\n'
+    bot.send_message(cid, help_text)
 
 
-# @bot.message_handler(commands=['start'])
-# def command_start(message):
-#     cid = message.chat.id
-#     bot.send_message(
-#         cid,
-#         "Welcome to Memecoins Trading Toolkit!\nType /help to find all commands."
-#     )
+@bot.message_handler(commands=['ping', 'p'])
+def command_ping(message):
+    if message.chat.id != int(OWNER_ID):
+        bot.reply_to(message, "Sorry you are not allowed to use this command!")
+    else:
+        start_time = time.time()
+        ping = modules.get_running_time(start_time)
+        bot.reply_to(message, "PONG! Running time: {:.3f} s.".format(ping))
 
-# @bot.message_handler(commands=['help'])
-# def command_help(message):
-#     cid = message.chat.id
-#     help_text = "The following commands are available: \n"
-#     for key in modules.COMMANDS:
-#         help_text += '/' + key + ': '
-#         help_text += modules.COMMANDS[key] + '\n'
-#     bot.send_message(cid, help_text)
 
-# @bot.message_handler(commands=['ping', 'p'])
-# def command_ping(message):
-#     if message.chat.id != int(OWNER_ID):
-#         bot.reply_to(message, "Sorry you are not allowed to use this command!")
-#     else:
-#         start_time = time.time()
-#         ping = modules.get_running_time(start_time)
-#         bot.reply_to(message, "PONG! Running time: {:.3f} s.".format(ping))
+@bot.message_handler(func=lambda message: modules.is_command(message.text))
+def command_unknown(message):
+    command = str(message.text).split()[0]
+    bot.reply_to(
+        message,
+        "Sorry, {} command not found!\nPlease use /help to find all commands.".
+        format(command))
 
-# @bot.message_handler(func=lambda message: modules.is_command(message.text))
-# def command_unknown(message):
-#     command = str(message.text).split()[0]
-#     bot.reply_to(
-#         message,
-#         "Sorry, {} command not found!\nPlease use /help to find all commands.".
-#         format(command))
 
 if __name__ == '__main__':
     app.run(threaded=False)
