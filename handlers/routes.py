4a85cb9b4657355d883a0abbc85702239f30aaed
gordonjun <gordonoh@yahoo.com.sg>
2024-12-01 18:32:51 +0800

    fix

--- handlers/routes.py
+++ handlers/routes.py
@@ -11,3 +11,10 @@ def configure_routes(app, bot):
         hello = modules.hello()
         content = modules.content()
         return render_template("index.html", hello=hello, content=content)
+
+    @app.route('/webhook', methods=['POST'])
+    def webhook():
+        update = telebot.types.Update.de_json(
+            request.stream.read().decode("utf-8"))
+        bot.process_new_updates([update])
+        return "ok", 200
