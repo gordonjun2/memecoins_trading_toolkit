31bf831bdb151e939f1bc6d9e76049f872f1c7a3
gordonjun <gordonoh@yahoo.com.sg>
2024-12-01 14:22:36 +0800

    fix webhook

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from flask import Flask
+from flask import Flask, request
 import telegram
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -10,6 +10,10 @@ BOT_TOKEN = TELEGRAM_BOT_TOKEN or os.getenv('TELEGRAM_BOT_TOKEN')
 APP_URL = VERCEL_APP_URL or os.getenv('VERCEL_APP_URL')
 CHAT_ID = TEST_TG_CHAT_ID or os.getenv('TEST_TG_CHAT_ID')
 bot = telegram.Bot(token=BOT_TOKEN)
+commands = [
+    telegram.BotCommand("status", "Check bot status"),
+]
+bot.set_my_commands(commands)
 
 app = Flask(__name__)
 
@@ -22,19 +26,36 @@ def home():
     return 'Bot is running.'
 
 
-@app.route('/send_message')
+@app.route('/status')
 def send_message():
+    if not CHAT_ID:
+        return "TEST_TG_CHAT_ID is not set."
     bot.send_message(chat_id=CHAT_ID, text="Hello from Flask! Bot is running.")
     return "Message sent!"
 
 
-@app.route('/set_webhook', methods=['GET', 'POST'])
-def set_webhook():
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=APP_URL, HOOK=BOT_TOKEN))
-    if s:
-        return "Webhook setup successful"
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    try:
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+        if update.message and update.message.text == '/status':
+            bot.send_message(chat_id=CHAT_ID,
+                             text="Hello from Flask! Bot is running.")
+
+    except Exception as e:
+        print(f"Error handling update: {e}")
+
+    return 'OK'
+
+
+@app.before_first_request
+def setup_webhook():
+    success = bot.setWebhook(f"{APP_URL}/webhook")
+    if success:
+        print("Webhook successfully set!")
     else:
-        return "Webhook setup failed"
+        print("Webhook setup failed.")
 
 
 if __name__ == '__main__':
