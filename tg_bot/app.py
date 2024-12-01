e2447c7b747c068e5ae2f8924a2ac64b327e877a
gordonjun <gordonoh@yahoo.com.sg>
2024-12-01 16:22:20 +0800

    testing

--- tg_bot/app.py
+++ tg_bot/app.py
@@ -39,43 +39,39 @@ def webhook():
     return "ok", 200
 
 
-@bot.message_handler(commands=['start'])
-def command_start(message):
-    cid = message.chat.id
-    bot.send_message(
-        cid,
-        "Welcome to Memecoins Trading Toolkit!\nType /help to find all commands."
-    )
+# @bot.message_handler(commands=['start'])
+# def command_start(message):
+#     cid = message.chat.id
+#     bot.send_message(
+#         cid,
+#         "Welcome to Memecoins Trading Toolkit!\nType /help to find all commands."
+#     )
 
+# @bot.message_handler(commands=['help'])
+# def command_help(message):
+#     cid = message.chat.id
+#     help_text = "The following commands are available: \n"
+#     for key in modules.COMMANDS:
+#         help_text += '/' + key + ': '
+#         help_text += modules.COMMANDS[key] + '\n'
+#     bot.send_message(cid, help_text)
 
-@bot.message_handler(commands=['help'])
-def command_help(message):
-    cid = message.chat.id
-    help_text = "The following commands are available: \n"
-    for key in modules.COMMANDS:
-        help_text += '/' + key + ': '
-        help_text += modules.COMMANDS[key] + '\n'
-    bot.send_message(cid, help_text)
+# @bot.message_handler(commands=['ping', 'p'])
+# def command_ping(message):
+#     if message.chat.id != int(OWNER_ID):
+#         bot.reply_to(message, "Sorry you are not allowed to use this command!")
+#     else:
+#         start_time = time.time()
+#         ping = modules.get_running_time(start_time)
+#         bot.reply_to(message, "PONG! Running time: {:.3f} s.".format(ping))
 
-
-@bot.message_handler(commands=['ping', 'p'])
-def command_ping(message):
-    if message.chat.id != int(OWNER_ID):
-        bot.reply_to(message, "Sorry you are not allowed to use this command!")
-    else:
-        start_time = time.time()
-        ping = modules.get_running_time(start_time)
-        bot.reply_to(message, "PONG! Running time: {:.3f} s.".format(ping))
-
-
-@bot.message_handler(func=lambda message: modules.is_command(message.text))
-def command_unknown(message):
-    command = str(message.text).split()[0]
-    bot.reply_to(
-        message,
-        "Sorry, {} command not found!\nPlease use /help to find all commands.".
-        format(command))
-
+# @bot.message_handler(func=lambda message: modules.is_command(message.text))
+# def command_unknown(message):
+#     command = str(message.text).split()[0]
+#     bot.reply_to(
+#         message,
+#         "Sorry, {} command not found!\nPlease use /help to find all commands.".
+#         format(command))
 
 if __name__ == '__main__':
     app.run(threaded=False)
